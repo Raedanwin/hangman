af084030faaee814d027ff2e5f0d31c30232a9cc
Raedanwin <Raedanwin@gmail.com>
2020-09-08 21:53:32 -0400

    underscores and start of display

--- Hangman.js
+++ Hangman.js
@@ -1,4 +1,6 @@
 const wordArray = ['Apple', 'Human', 'Animal', 'Pikachu', 'Abyss', 'Guitar', 'Elephant'];
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+let guesses = [];
 
 let wordChoice = () => {
     let newWord = wordArray[Math.floor(Math.random() * wordArray.length)]
@@ -10,13 +12,22 @@ let calculateUnderscores = () => {
     for (let i = 0; i <= chosenWord.length; i++) {
         let under = document.createElement('div');
         under.innerText = '_';
-        let underDiv = document.getElementById('underscores')
+        under.setAttribute('class', 'letters');
+        under.setAttribute('id', i);
+        let underDiv = document.getElementById('underscores');
         console.log(under);
         underDiv.appendChild(under);
+    } 
+}
+
+let displayLetters = () => {
+    for (let j = 0; j <= chosenWord.length; j++) {
+        let word = [];
+        word = chosenWord.length;
+        console.log(word);
     }
 }
-// let underscoreCount = 
-//calculateUnderscores();
+
  
 let displayWord = () => {
     document.getElementById('display').innerHTML = chosenWord;
